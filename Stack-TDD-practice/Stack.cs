6904bfb1a55eed6dcc14bd928a9c2e55ca694567
Fabian Fabro <fabrofabian@gmail.com>
2019-10-01 15:52:34 -0700

    added Stack check test

---
+++ Stack-TDD-practice/Stack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_TDD_practice
+{
+    public class Stack<T>
+    {
+        #region Members
+        private T[] stackArray;
+        private int maxLength;
+        #endregion
+
+        #region Properties
+        public int Size { get; private set; }
+        #endregion
+
+        #region Constructor
+        public Stack(int length)
+        {
+            maxLength = length;
+            stackArray = new T[length];
+        }
+        #endregion
+    }
+}
---
+++ Stack-Tests/StackTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace Stack_Tests
+{   
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void Creation()
+        {
+            Stack<int> s = new Stack<int>(3);
+            Assert.AreEqual(0, s.Count);
+        }
+
+        [Test]
+        public void Push_Pop()
+        {
+            Stack<int> s = new Stack<int>(3);
+            s.Push(1);
+            s.Push(2);
+            s.Push(3);
+            int value = s.Pop();
+
+            Assert.AreEqual(3, value);
+            Assert.AreEqual(2, s.Count);
+        }
+    }
+}
